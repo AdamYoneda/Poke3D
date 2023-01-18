b8d16936027ccaf3432e1c7ed2de261f58cdcdc1
Adam Yoneda <adamyone.ttlife@gmail.com>
2023-01-19 00:11:21 +0900

    Completed

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -21,19 +21,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        
+        // Lighting
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARImageTrackingConfiguration()
+        let configuration = ARWorldTrackingConfiguration()
         
         if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
             
-            configuration.trackingImages = imageToTrack
+            configuration.detectionImages = imageToTrack
             
-            configuration.maximumNumberOfTrackedImages = 1
+            configuration.maximumNumberOfTrackedImages = 2
             
             print("Images successfully added")
         }
@@ -54,18 +57,40 @@ class ViewController: UIViewController, ARSCNViewDelegate {
    
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-        if let imageAnchor = anchor as? ARImageAnchor {
-            let plane = SCNPlane(
-                width: imageAnchor.referenceImage.physicalSize.width,
-                height: imageAnchor.referenceImage.physicalSize.height
-            )
-            plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
-            let planeNode = SCNNode(geometry: plane)
-            planeNode.eulerAngles.x = -.pi / 2
-            node.addChildNode(planeNode)
+        DispatchQueue.main.async {
+            if let imageAnchor = anchor as? ARImageAnchor {
+                let plane = SCNPlane(
+                    width: imageAnchor.referenceImage.physicalSize.width,
+                    height: imageAnchor.referenceImage.physicalSize.height
+                )
+                plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+                let planeNode = SCNNode(geometry: plane)
+                planeNode.eulerAngles.x = -.pi / 2
+                node.addChildNode(planeNode)
+                // Create 3D Object
+                if imageAnchor.referenceImage.name == "Eevee-card" {
+                    guard let pokeScene = SCNScene(named: "artisan.scnassets/eevee2.scn") else {
+                        fatalError(".referenceImage.name = nil, or any other name, or any other problem")
+                    }
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        pokeNode.eulerAngles.x = .pi / 2
+                        planeNode.addChildNode(pokeNode)
+                    }
+                }
+                if imageAnchor.referenceImage.name == "Oddish-card" {
+                    guard let pokeScene = SCNScene(named: "artisan.scnassets/oddish.scn") else {
+                        fatalError(".referenceImage.name = nil, or any other name, or any other problem")
+                    }
+                    if let pokeNode = pokeScene.rootNode.childNodes.first {
+                        pokeNode.eulerAngles.x = .pi / 2
+                        planeNode.addChildNode(pokeNode)
+                    }
+                }
+            }
         }
         return node
     }
+    
     
 }
  
